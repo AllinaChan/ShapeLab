df7354bc0af73c9b434a2f5a67f2d45d5c59e316
Samooooooo <Smei0748@bths.edu>
2018-10-16 14:36:46 -0400

    ETriangle

--- src/Shape.java
+++ src/Shape.java
@@ -1,11 +1,12 @@
 public abstract class Shape {
     private String type;
 
-    public Shape(String type) {
+    public Shape(String type)
+    {
         this.type=type;
     }
     public abstract double getArea();
-    public abstract double getPreimeter();
+    public abstract double getPerimeter();
     public String toString()
     {
         return "I am a " + type + ".";
