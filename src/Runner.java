cb498c88c174ac4675d1ddc5bb7846d92e54d363
Samooooooo <Smei0748@bths.edu>
2018-10-18 14:54:54 -0400

    Notes

--- src/Runner.java
+++ src/Runner.java
@@ -3,8 +3,9 @@ public class Runner {
         Shape shape1 = new Rectangle(10, 5);
         Shape shape2 = new ETriangle(5);
         Shape shape3 = new Circle(10);
+        Shape shape4 = new ITriangle(3,8);
 
-        Shape[] shapes = {shape1, shape2, shape3};
+        Shape[] shapes = {shape1, shape2, shape3, shape4};
 
         for (Shape cur : shapes) {
             System.out.println(cur);
