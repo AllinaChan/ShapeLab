ce8dd85a060982f994c3b39377476e03b5b457a1
Samooooooo <Smei0748@bths.edu>
2018-10-18 14:36:28 -0400

    Notes

--- src/ETriangle.java
+++ src/ETriangle.java
@@ -1,6 +1,13 @@
 public class ETriangle extends Shape
 {
     private int side;
+
+    /**
+     * getArea returns the Area of the equilaterial triangle
+     * getPerimeter returns the Perimeter of the equilateral triangle
+     *
+     * @param side one of the 3 equal sides of the triangle
+     */
     public ETriangle(int side)
     {
         super("ETriangle");
